44b49d7b84d2a14fb37a705439fe530092e4f971
Andrew Thomas <athomik79@gmail.com>
2020-03-04 18:16:22 -0500

    adding .js files for all pages, updated css and html for final app

--- game.js
+++ game.js
@@ -1,128 +1,114 @@
-// DOM query for question and choices
-var question = document.getElementById("question");
-var choices = Array.from(document.getElementsByClassName("choice-text"));
-
-// counter and score variables
-var questionCounterText = document.getElementById("questionCounter");
-var scoreText = document.getElementById("score");
-// console.log(choices);
-
-// Variables
-let currentQuestions = {};
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
+let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
-let availableQuestions = [];
+let availableQuesions = [];
 
-// Questions Array (questions as objects)
-let questions = [
-    {
-        question: "Inside which HTML element do we put the JavaScript?",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-    },
-    {
-        question: "What is the correct syntax for referring to an extenal script called 'xxx.'js?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3
-    },
-    {
-        question: "How do you write 'Hello World' in an alert box?",
-        choice1: "<msgBox('Hello World');",
-        choice2: "<alertBox('Hello World');",
-        choice3: "<msg('Hello World');",
-        choice4: "<alert('Hello World');",
-        answer: 4
-    }
-];
+let questions = [];
 
-// Constants
+fetch(
+  "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
+)
+  .then(res => {
+    return res.json();
+  })
+  .then(loadedQuestions => {
+    console.log(loadedQuestions.results);
+    questions = loadedQuestions.results.map(loadedQuestion => {
+      const formattedQuestion = {
+        question: loadedQuestion.question
+      };
+
+      const answerChoices = [...loadedQuestion.incorrect_answers];
+      formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+      answerChoices.splice(
+        formattedQuestion.answer - 1,
+        0,
+        loadedQuestion.correct_answer
+      );
+
+      answerChoices.forEach((choice, index) => {
+        formattedQuestion["choice" + (index + 1)] = choice;
+      });
+
+      return formattedQuestion;
+    });
+
+    startGame();
+  })
+  .catch(err => {
+    console.error(err);
+  });
+
+//CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
 startGame = () => {
-    questionCounter = 0;
-    score = 0;
-    // spread operator
-    availableQuestions = [...questions];
-    // console.log(availableQuestions);
-    getNewQuestion();
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
+  game.classList.remove("hidden");
+  loader.classList.add("hidden");
 };
 
-// New Question Function
 getNewQuestion = () => {
-
-    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("mostRecentScore", score);
     //go to the end page
-    return window.location.assign("/end.html");
-    }  
-    questionCounter++;
-    // dynamic question counter text
-    questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
+    return window.location.assign("end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  //Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
-    var questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerHTML = currentQuestion.question;
 
-    // For each data-number property
-    choices.forEach( choice => {
-        var number = choice.dataset["number"];
-        choice.innerText = currentQuestion["choice" + number];
-    });
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerHTML = currentQuestion["choice" + number];
+  });
 
-    // Splice for quesiton index
-    availableQuestions.splice(questionIndex, 1);
-    // Allow users to answer after loaded
-    acceptingAnswers = true;
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
 };
 
-// onclick event for choices
 choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        // console.log(e.target);
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
 
-        if(!acceptingAnswers) return;
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
 
-        acceptingAnswers = false;
-        var selectedChoice = e.target;
-        var selectedAnswer = selectedChoice.dataset["number"];
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-        // Apply class to answer
-        var classToApply = "incorrect";
-            if (selectedAnswer == currentQuestion.answer) {
-                classToApply = "correct";
-            }
-            // console.log(classToApply);
+    if (classToApply === "correct") {
+      incrementScore(CORRECT_BONUS);
+    }
 
-            // call score bonus function
-            if(classToApply === "correct") {
-                incrementScore(CORRECT_BONUS);
-            }        // console.log(selectedAnswer == currentQuestions.answer);
+    selectedChoice.parentElement.classList.add(classToApply);
 
-        // add and remove colors for correct and incorrect answers
-        selectedChoice.parentElement.classList.add(classToApply);
-        
-        setTimeout( () => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 1000);
-
-    });
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
 });
 
-// score function
 incrementScore = num => {
-    score+= num;
-    scoreText.innerText = score;
-}
-
- 
-
-// Start Game Function
-startGame();
+  score += num;
+  scoreText.innerText = score;
+};
