b3751294bd2af267e5ce3e8c8d9c54d3bc151e0c
Andrew Thomas <athomik79@gmail.com>
2020-03-05 19:20:13 -0500

    fixed dynamic links to high score page

--- end.js
+++ end.js
@@ -3,9 +3,9 @@ var saveScoreBtn = document.getElementById("saveScoreBtn");
 var finalScore = document.getElementById("finalScore");
 var mostRecentScore = localStorage.getItem("mostRecentScore");
 
-var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
-var MAX_HIGH_SCORES = 5;
+const MAX_HIGH_SCORES = 5;
 
 finalScore.innerText = mostRecentScore;
 
@@ -26,5 +26,5 @@ saveHighScore = e => {
   highScores.splice(5);
 
   localStorage.setItem("highScores", JSON.stringify(highScores));
-  window.location.assign("/");
+  window.location.assign("highscores.html");
 };
--- game.js
+++ game.js
@@ -9,20 +9,33 @@ let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
-let questions = [];
-
-fetch("questions.json")
-  .then(res => {
-    return res.json();
-  })
-  .then(loadedQuestions => {
-    console.log(loadedQuestions);
-    questions = loadedQuestions;
-    startGame();
-  })
-  .catch(err => {
-    console.error(err);
-  });
+let questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript??",
+    choice1: "<script>",
+    choice2: "<javascript>",
+    choice3: "<js>",
+    choice4: "<scripting>",
+    answer: 1
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choice1: "<script href='xxx.js'>",
+    choice2: "<script name='xxx.js'>",
+    choice3: "<script src='xxx.js'>",
+    choice4: "<script file='xxx.js'>",
+    answer: 3
+  },
+  {
+    question: " How do you write 'Hello World' in an alert box?",
+    choice1: "msgBox('Hello World');",
+    choice2: "alertBox('Hello World');",
+    choice3: "msg('Hello World');",
+    choice4: "alert('Hello World');",
+    answer: 4
+  }
+];
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
@@ -51,7 +64,7 @@ getNewQuestion = () => {
   question.innerText = currentQuestion.question;
 
   choices.forEach(choice => {
-    var number = choice.dataset["number"];
+    const number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
   });
 
@@ -87,3 +100,5 @@ incrementScore = num => {
   score += num;
   scoreText.innerText = score;
 };
+
+startGame();
